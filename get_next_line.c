d113b476cffac81e807dc6090193e5e448361b80
Flovln <florent.violin@gmail.com>
2016-01-12 21:29:13 +0100

    work in progress / works well

--- get_next_line.c
+++ get_next_line.c
@@ -18,20 +18,19 @@ static char		*ft_mem_concat(char *str, char *buf)
 {
 	char *tmp;
 
-	tmp = ft_strnew(BUFF_SIZE + 1);
 	tmp = ft_strjoin(str, buf);
 	return (tmp);
 }
 
-static int		ft_get_line(char **line, char *overf)
+static int		ft_get_line(char **line, char *buf, char *overf)
 {
 	char *tmp;
 
-	if ((tmp = ft_strchr(overf, '\n')))
+	if ((tmp = ft_strchr(buf, '\n')))
 	{
 		*tmp = '\0';
-		*line = ft_strdup(overf);
-		ft_memmove(overf, tmp + 1, ft_strlen(tmp + 1) + 1);
+		*line = ft_strdup(buf);
+		ft_memmove(overf, tmp + 1, ft_strlen(tmp + 1) + 1); //recuperer l'overflow
 		return (1);
 	}
 	return (0);
@@ -39,25 +38,31 @@ static int		ft_get_line(char **line, char *overf)
 
 int				get_next_line(int const	fd, char **line)
 {
-	static char *overf;
+	static char overf[BUFF_SIZE + 1];
 	char		buf[BUFF_SIZE + 1];
-	//char		*str;
+	char		*str;
 	int			ret;
 
-	overf = ft_strnew(BUFF_SIZE + 1);
 	ft_bzero(buf, BUFF_SIZE);
+	if (ft_get_line(line, overf, overf) == 1)
+		return (1);
+	str = ft_strdup(overf);
+	ft_bzero(buf, BUFF_SIZE + 1);
 	while ((ret = read(fd, buf, BUFF_SIZE)) != 0) // tant que l'on a pas atteint EOF (= 0)
 	{
 		if (ret == -1)
 			return (-1);
 		//printf("\nRead %d bytes\n\nContent = ||%s||\n", ret, buf);
-		overf = ft_mem_concat(overf, buf);
-		ft_get_line(line, overf);
+		buf[ret] = '\0';
+		str = ft_mem_concat(str, buf);
+		if (ft_get_line(line, str, overf) == 1)
+			return (1);
 		//ft_putstr("\nContenu de line dans gnl : ");
 		//ft_putstr(*line);
-		printf("\nContenu de line dans gnl : %s\n ", *line);
+		//printf("\nContenu de line dans gnl : %s\n ", *line);
 		//printf("\nContenu de overf dans gnl : %s\n ", overf);
 	}
 	//free(str);
+	*line = NULL; // OU *line = ft_strnew(1)
 	return (0);
 }
