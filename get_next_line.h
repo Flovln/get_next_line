1c272770dbf159eda9623ec26829a7cbd6b01ae8
Florent VIOLIN <fviolin@e1r1p14.42.fr>
2016-01-13 11:27:57 +0100

    stdio to be deleted / printf testing

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 18:07:39 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/07 13:11:30 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/13 11:27:05 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 5
+# define BUFF_SIZE 99999
 
 #include "libft/libft.h"
-#include <stdlib.h> //////////////////TEST
-#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h> ////////////TEST A SUPPRIMER /////
 #include <unistd.h>
 #include <fcntl.h>
 
