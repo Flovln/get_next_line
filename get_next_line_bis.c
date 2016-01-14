f334a800072bfae5c0110d9b6a5d07638b65946d
Florent VIOLIN <fviolin@e1r5p21.42.fr>
2016-01-14 14:37:17 +0100

    backup file

--- get_next_line_bis.c
+++ get_next_line_bis.c
@@ -6,56 +6,74 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 18:07:22 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/13 11:29:52 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/14 14:23:17 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* gestion memoire + concatenation */
-
 static char		*ft_mem_concat(char *str, char *buf)
 {
 	char *tmp;
 
-	tmp = ft_strnew(BUFF_SIZE + 1);
 	tmp = ft_strjoin(str, buf);
+	free(str);
+	str = NULL;
 	return (tmp);
 }
 
-static int		ft_get_line(char **line, char *str)
+static int		ft_get_line(char **line, char *buf, char *overf)
 {
 	char *tmp;
 
-	if ((tmp = ft_strchr(str, '\n')))
+	if ((tmp = ft_strchr(buf, '\n')))
 	{
 		*tmp = '\0';
-		*line = ft_strdup(str);
-		ft_memmove(str, tmp + 1, ft_strlen(tmp + 1) + 1);
+		*line = ft_strdup(buf);
+		ft_memmove(overf, tmp + 1, ft_strlen(tmp + 1) + 1);
 		return (1);
 	}
 	return (0);
 }
 
-int				get_next_line(int const	fd, char **line)
+int				get_next_line(int const fd, char **line)
 {
-	static char *overf;
+	static char overf[BUFF_SIZE + 1];
 	char		buf[BUFF_SIZE + 1];
 	char		*str;
 	int			ret;
+	/*static int 	num;
+	static t_input save[MAX_FD];
 
-	overf = ft_strnew(BUFF_SIZE + 1);
-	ft_bzero(buf, BUFF_SIZE);
-	while ((ret = read(fd, buf, BUFF_SIZE)) != 0) // tant que l'on a pas atteint EOF (= 0)
+	index = fd % MAX_FD;
+	save[index].overf
+	if (num != fd)
+		ft_bzero(overf, BUFF_SIZE + 1);
+	num = fd;*/
+	if (ft_get_line(line, overf, overf) == 1)
+		return (1);
+	str = ft_strdup(overf);
+	ft_bzero(buf, BUFF_SIZE + 1);
+	ft_bzero(overf, BUFF_SIZE + 1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) != 0)
 	{
 		if (ret == -1)
+		{
+			ft_bzero(overf, BUFF_SIZE + 1);
 			return (-1);
-		//printf("\nRead %d bytes\n\nContent = ||%s||\n", ret, buf);
+		}
+		buf[ret] = '\0';
 		str = ft_mem_concat(str, buf);
-		ft_get_line(line, str);
-		printf("\nContenu de line dans gnl : %s\n ", *line);
-		printf("\nContenu de str dans gnl : %s\n ", str);
+		if (ft_get_line(line, str, overf) == 1)
+		{
+			free(str);
+			str = NULL;
+			return (1);
+		}
 	}
-	//free(str);
+	ft_bzero(overf, BUFF_SIZE + 1);
+	*line = str;
+	free(str);
+	str = NULL;
 	return (0);
 }
