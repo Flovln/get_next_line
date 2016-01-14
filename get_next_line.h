965a8bcf29dbe3a0914e19b091413be4c64a5a8b
Florent VIOLIN <fviolin@e1r5p21.42.fr>
2016-01-14 15:42:30 +0100

    struct sketch ofr multiple file descriptor

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,26 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 18:07:39 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/13 11:27:05 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/14 15:41:22 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 99999
+# define BUFF_SIZE 7
+# define MAX_FD 20
 
-#include "libft/libft.h"
-#include <stdlib.h>
-#include <stdio.h> ////////////TEST A SUPPRIMER /////
-#include <unistd.h>
-#include <fcntl.h>
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+/*typedef struct s_input
+{
+	char		overf[BUFF_SIZE + 1];
+	int			fd;
+}				t_input;
+*/
 
 int		get_next_line(int const fd, char **line);
 
