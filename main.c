50dff51386df2338da8228b1d5c2fc4c971bdb8d
Florent VIOLIN <fviolin@e1r1p14.42.fr>
2016-01-15 10:39:20 +0100

    main test

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/15 09:44:27 by fviolin           #+#    #+#             */
+/*   Updated: 2016/01/15 10:06:35 by fviolin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 
 int		main(int ac, char **av)
 {
-	int fd;
-	int i;
-	char *line;
+	int		fd;
+	int		i;
+	char	*line;
 
 	line = NULL;
 	fd = open(av[1], O_RDONLY);
-	if (fd == - 1)
+	if (fd == -1)
 		return (-1);
 	i = 1;
 	while (i == 1)
