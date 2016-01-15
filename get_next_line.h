24620ffee5d873e041ab668953f8e2836c0f6059
Florent VIOLIN <fviolin@e1r1p14.42.fr>
2016-01-15 10:38:08 +0100

    norme OK / .h ALL OK

--- get_next_line.h
+++ get_next_line.h
@@ -6,26 +6,18 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 18:07:39 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/14 15:41:22 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/15 10:34:44 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 7
-# define MAX_FD 20
+# define BUFF_SIZE 73
 
 # include "libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
-
-/*typedef struct s_input
-{
-	char		overf[BUFF_SIZE + 1];
-	int			fd;
-}				t_input;
-*/
 
 int		get_next_line(int const fd, char **line);
 
