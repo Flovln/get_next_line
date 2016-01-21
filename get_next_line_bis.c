0cfe97891c8e4ec0c0d752350be1aa15a758b25c
Flovln <florent.violin@gmail.com>
2016-01-21 21:29:01 +0100

    work in progress / backup file

--- get_next_line_bis.c
+++ get_next_line_bis.c
@@ -6,19 +6,19 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 18:07:22 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/14 16:41:42 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/15 10:51:18 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 static char		*ft_mem_concat(char *str, char *buf)
 {
 	char *tmp;
 
 	tmp = ft_strjoin(str, buf);
-	free(str);
-	str = NULL;
+	ft_strdel(&str);
 	return (tmp);
 }
 
@@ -33,37 +33,39 @@ static int		ft_get_line(char **line, char *buf, char *overf)
 		ft_memmove(overf, tmp + 1, ft_strlen(tmp + 1) + 1);
 		return (1);
 	}
+	else if ((tmp = ft_strchr(buf, '\0')))
+	{
+		*line = ft_strdup(buf);
+	}
 	return (0);
 }
 
 int				get_next_line(int const fd, char **line)
 {
-	static char overf[BUFF_SIZE + 1];
+	static char overf[255][BUFF_SIZE + 1];
 	char		buf[BUFF_SIZE + 1];
 	char		*str;
 	int			ret;
 
-	if (ft_get_line(line, overf, overf) == 1)
+	if (fd < 0)
+		return (-1);
+	if (ft_get_line(line, overf[fd], overf[fd]) == 1)
 		return (1);
-	str = ft_strdup(overf);
+	str = ft_strdup(overf[fd]);
 	ft_bzero(buf, BUFF_SIZE + 1);
-	ft_bzero(overf, BUFF_SIZE + 1);
 	while ((ret = read(fd, buf, BUFF_SIZE)) != 0)
 	{
 		if (ret == -1)
 			return (-1);
 		buf[ret] = '\0';
 		str = ft_mem_concat(str, buf);
-		if (ft_get_line(line, str, overf) == 1)
+		if (ft_get_line(line, str, overf[fd]) == 1)
 		{
-			free(str);
-			str = NULL;
+			ft_strdel(&str);
 			return (1);
 		}
 	}
-	ft_bzero(overf, BUFF_SIZE + 1);
-	*line = str; // si on nous passe 1 string sans \n (avec EOF a la fin) alors on la stock directement dans *line,
-	free(str);
-	str = NULL;
-	return (0);
+	//*line = str;
+	return (ft_strlen(*line) != 0);
+	//return (0);
 }
