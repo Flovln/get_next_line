07f03250de86f8c5624b77986945a08f0eab2ad9
Florent VIOLIN <fviolin@e1r1p14.42.fr>
2016-01-27 10:44:02 +0100

    Freed line for leaks checking

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fviolin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/15 09:44:27 by fviolin           #+#    #+#             */
-/*   Updated: 2016/01/20 09:47:43 by fviolin          ###   ########.fr       */
+/*   Updated: 2016/01/24 16:13:00 by fviolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int		main(int ac, char **av)
 
 	line = NULL;
 	fd = open(av[1], O_RDONLY);
+	if (ac > 1)
 	if (fd == -1)
 		return (-1);
 	ret_value = 1;
@@ -28,6 +29,7 @@ int		main(int ac, char **av)
 	{
 		ret_value = get_next_line(fd, &line);
 		printf("%d || %s\n", ret_value, line);
+		ft_strdel(&line);
 	}
 	close(fd);
 	return (0);
